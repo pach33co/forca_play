63188ec42fde5c1b96c94d0abac706b0522ee4f6
Matheus Pacheco <pachecmm@gmail.com>
2026-08-19 20:04:36 -0300

    feat: instala a font poppins no projeto

--- App.tsx
+++ App.tsx
@@ -1,20 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+
+  const [loaded, error] = useFonts({
+    PoppinsRegular: require("./assets/fonts/Poppins/Poppins-Regular.ttf"),
+    PoppinsBold: require("./assets/fonts/Poppins/Poppins-Bold.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text style={{ fontFamily: 'PoppinsBold' }}>Hello world</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
