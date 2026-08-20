8c1242acbff035388b50d4d7bd08a6bb0139645e
Matheus Pacheco <pachecmm@gmail.com>
2026-08-20 06:55:32 -0300

    feat: cria os tokens de cores do styles

---
+++ src/shared/styles/Styles.ts
@@ -0,0 +1,31 @@
+
+
+export const styles = {
+    colors: {
+        background: '#120E07',
+        paper: '#332714',
+        text: '#F8F3ED',
+        primary: '#FFC000',
+        alert: '#FFD700',
+        primaryText: '#181816',
+        correct: '#228B22',
+        wrong: '#FF3131'
+    },
+    corner: {
+        small: 4,
+        normal: 8,
+        large: 16
+    },
+    fonts: {
+        sizes: {
+            small: 12,
+            body: 14,
+            large: 16,
+            extraLarge: 20
+        },
+        family: {
+            bold: 'PoppinsBold',
+            regular: 'PoppinsRegular'
+        },
+    },
+};
